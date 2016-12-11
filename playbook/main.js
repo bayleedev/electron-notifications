d4c1bc3385d4a2a64fd15192f0ce81bb13e736a6
David K. Coleman <david_coleman_007@hotmail.com>
2016-12-10 20:57:21 -0800

    remove dev tools in main.js

--- playbook/main.js
+++ playbook/main.js
@@ -5,7 +5,6 @@ let mainWindow = null
 app.on('ready', () => {
   mainWindow = new BrowserWindow({width: 800, height: 600})
   mainWindow.loadURL('file://' + __dirname + '/playbook.html')
-  mainWindow.webContents.openDevTools()
   mainWindow.on('closed', () => {
     mainWindow = null
   })
